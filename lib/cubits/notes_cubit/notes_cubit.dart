5968a0aad7429562f7e1535ab187afd221976588
Hussein Issa <www.hussein2001@gmail.com>
2024-05-10 23:41:28 +0300

    display notes part one

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -1,5 +1,5 @@
 import 'package:bloc/bloc.dart';
-import 'package:hive_flutter/hive_flutter.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
 import 'package:notes_app/constants.dart';
 import 'package:notes_app/models/note_model.dart';
@@ -8,18 +8,9 @@ part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
-
+  List<NoteModel>? notesList;
   fetchAllNotes() {
-    try {
-      var notesBox = Hive.box<NoteModel>(kNotesBox);
-      List<NoteModel> notesList = notesBox.values.toList();
-      emit(NotesSuccess(
-        notesList: notesList,
-      ));
-    } catch (e) {
-      emit(NotesFailure(
-        errMessage: e.toString(),
-      ));
-    }
+    var notesBox = Hive.box<NoteModel>(kNotesBox);
+    notesList = notesBox.values.toList();
   }
 }
