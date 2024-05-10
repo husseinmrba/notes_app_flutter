71a6d99ae3733c53d2d0c0effaeea55abf78dcea
Hussein Issa <www.hussein2001@gmail.com>
2024-05-11 00:07:02 +0300

    delete note

--- lib/widgets/custom_note_item.dart
+++ lib/widgets/custom_note_item.dart
@@ -47,7 +47,9 @@ class CustomNoteItem extends StatelessWidget {
                 ),
               ),
               trailing: IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  note.delete();
+                },
                 icon: const Icon(
                   FontAwesomeIcons.trash,
                   color: Colors.black,
